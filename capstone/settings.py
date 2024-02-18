e1907ae0fc90d58f87224cbeee13798eff6ca05f
Siddhant Sharma <86948743+Sidd2024@users.noreply.github.com>
2024-02-18 16:36:17 +0530

    Update settings.py

--- capstone/settings.py
+++ capstone/settings.py
@@ -12,6 +12,8 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 
 from pathlib import Path
 import os
+import django_heroku
+import dj_database_url
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -147,4 +149,5 @@ EMAIL_USE_TLS = True
 ALLOWED_HOSTS = [
     '*'
 ]
+django_heroku.settings(locals())
 
