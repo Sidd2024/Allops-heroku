0098f7d2c0f8a8117143a835fd0759d26c893970
Siddhant Sharma <86948743+Sidd2024@users.noreply.github.com>
2024-03-04 10:05:55 +0530

    Update admin.py

--- Allops/admin.py
+++ Allops/admin.py
@@ -1,7 +1,16 @@
 from django.contrib import admin
 from .models import *
+from import_export import resources, fields
+from import_export.admin import ImportExportModelAdmin
+
+class opportunityResource(resources.ModelResource):
+    class Meta:
+        model = opportunity
+
+class opportunityAdmin(ImportExportModelAdmin):
+    resource_class = opportunityResource
 
 admin.site.register(User)
-admin.site.register(opportunity)
+admin.site.register(opportunity,opportunityAdmin)
 admin.site.register(save)
 admin.site.register(mails)
